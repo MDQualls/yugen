709b298422fad597424280af9001ca8b8e02ad39
MDQualls <mqualls@gmail.com>
2020-04-24 12:28:13 -0500

    style about page, add card css, utility css, start form css, start styling of login page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,23 +3,31 @@
 @section('content')
         <div class='row'>
 
-            <h3>Welcome to the Yugen Farm Blog</h3>
-            <p>
-                As of December 2019, we are located in beautiful Oklahoma City, Ok while we wait to move to New Mexico.
-                There’s me, Holly, and my husband Michael at the (future) farm, along with two dogs, Abigail and Onder, and our half feral cat, Castor.
-                We can be reached at <a href="#">yugenfarm@gmail.com</a>.
-                Michael built this site to document our journey, for ourselves and for others, as we move towards an off-grid, self-sufficient way of life using permaculture principles. We hope to show that once established, the farm is enjoyable, cheap and kind to the planet.
+            <h3 class="h3__title">Welcome to the Yugen Farm Web Presence</h3>
+            <p class="p__content--lead">
+                As of April, 2020 we are still located in Oklahoma City, Ok trying to outlast the Covid-19 Pandemic.
+                The original goal was the move to New Mexico to start our farming journey, but life and pandemics have
+                a way of changing things.  As of today, we are under contract to sell our house.  We are also under
+                contract to purchase a farm east of Oklahoma City.  While it is not New Mexico, it is land and we will
+                be able to fulfill our dream of sustainable farming.  If all goes as planned (finally), we should be
+                on our farm by the end of May, 2020.
             </p>
-
+            <p class="p__content--lead">
+                There’s me, Holly, and my husband Michael at the (future) farm, along with two dogs, Abigail and Onder,
+                and our half feral cat, Castor.  We can be reached at <a class="lead-link" href="#">yugenfarm@gmail.com</a>.
+                Michael built this site to document our journey, for ourselves and for others, as we move towards
+                a self-sufficient way of life using sustainable principles. We hope to show that
+                once established, the farm is enjoyable, cheap and kind to the planet.
+            </p>
         </div>
-        <div class='row'>
+        <div class='row bottom-margin-rem6 black-bottom-border'>
             <div class='col-1-of-4'>
 
-                <div class='team-card'>
+                <div class='image-card'>
                     <img src='{{url("images/holly1.jpg")}}' alt='' class='img-fluid'>
-                    <div class='team-overlay align-items-center'>
-                        <div class='team-detail'>
-                            <h4>Holly</h4>
+                    <div class='image-card__overlay'>
+                        <div class='image-card__detail'>
+                            <h4 class="h4__title">Holly</h4>
                             <span>Head Farmer and Lead Writer</span>
                         </div>
                     </div>
@@ -28,10 +36,10 @@
             </div><!--/col-->
             <div class='col-1-of-4'>
 
-                <div class='team-card'>
+                <div class='image-card'>
                     <img src='{{url("images/michael1.jpg")}}' alt='' class='img-fluid'>
-                    <div class='team-overlay align-items-center'>
-                        <div class='team-detail'>
+                    <div class='image-card__overlay'>
+                        <div class='image-card__detail'>
                             <h4>Michael</h4>
                             <span>Web Developer and Bee Keeper</span>
                         </div>
@@ -41,10 +49,10 @@
             </div><!--/col-->
             <div class='col-1-of-4'>
 
-                <div class='team-card'>
+                <div class='image-card'>
                     <img src='{{url("images/abbiandonder2.jpg")}}' alt='' class='img-fluid'>
-                    <div class='team-overlay align-items-center'>
-                        <div class='team-detail'>
+                    <div class='image-card__overlay'>
+                        <div class='image-card__detail'>
                             <h4>Abigail & Onder</h4>
                             <span>Security Detail</span>
                         </div>
@@ -54,10 +62,10 @@
             </div><!--/col-->
             <div class='col-1-of-4'>
 
-                <div class='team-card'>
+                <div class='image-card'>
                     <img src='{{url("images/castor.jpg")}}' alt='' class='img-fluid'>
-                    <div class='team-overlay align-items-center'>
-                        <div class='team-detail'>
+                    <div class='image-card__overlay'>
+                        <div class='image-card__detail'>
                             <h4>Castor</h4>
                             <span>The Bad Kitty</span>
                         </div>
@@ -67,22 +75,22 @@
             </div><!--/col-->
         </div>
         <div class='row'>
-            <div>
+            <div class="text-center bottom-margin-rem4">
                 <img src="{{url("images/yugen.gif")}}" alt="Yugen">
             </div>
-            <p>
-                <a target="_blank" href="http://en.wikipedia.org/wiki/Japanese_aesthetics">
+            <p class="p__content--lead text-center">
+                <a class="lead-link" target="_blank" href="http://en.wikipedia.org/wiki/Japanese_aesthetics">
                     <strong>Wikipedia article on Japanese Aesthetics</strong>
                 </a>
             </p>
-            <p>
+            <p class="p__content--lead text-center">
                 Yūgen (幽玄) is an important concept in traditional Japanese aesthetics. The exact translation of the word depends on the context. In the Chinese philosophical texts the term was taken from, yūgen meant “dim”, “deep” or “mysterious”. In the criticism of Japanese waka poetry, it was used to describe the subtle profundity of things that are only vaguely suggested by the poems.
 
                 Yūgen is said to mean “a profound, mysterious sense of the beauty of the universe… and the sad beauty of human suffering”.
 
                 Yūgen suggests that beyond what can be said but is not an allusion to another world. It is about this world, this experience.
             </p>
-            <p>
+            <p class="p__content--lead text-center black-bottom-border">
                 (Ortolani, 325). Ortolani, Benito. The Japanese Theatre. Princeton University Press: Princeton, 1995
             </p>
         </div>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,81 +1,81 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('partials.pagetitle')
-    <div class="container">
-        <section id="cover" class="mb70 mt-5" >
-            <div id="cover-caption">
-                <div class="container">
-                    <div class="col-md-8 ml-auto mr-auto">
-                        <div class="card">
-                            <div class="card-header">{{ __('Login') }}</div>
+    <div class="row">
+        <div class="card">
+            <div class="card__header card--dark">
+                {{ __('Login') }}
+            </div>
 
-                            <div class="card-body">
-                                <form method="POST" action="{{ route('login') }}">
-                                    @csrf
+            <div class="card__body">
 
-                                    <div class="form-group row">
-                                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
 
-                                        <div class="col-md-6">
-                                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <div class="form-group row">
+                        <label for="email"
+                               class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                                            @error('email')
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-                                    </div>
+                        <div class="col-md-6">
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                   name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                    <div class="form-group row">
-                                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
 
-                                        <div class="col-md-6">
-                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                    <div class="form-group row">
+                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                                            @error('password')
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-                                    </div>
+                        <div class="col-md-6">
+                            <input id="password" type="password"
+                                   class="form-control @error('password') is-invalid @enderror" name="password" required
+                                   autocomplete="current-password">
 
-                                    <div class="form-group row">
-                                        <div class="col-md-6 offset-md-4">
-                                            <div class="form-check">
-                                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
 
-                                                <label class="form-check-label" for="remember">
-                                                    {{ __('Remember Me') }}
-                                                </label>
-                                            </div>
-                                        </div>
-                                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-6 offset-md-4">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="remember"
+                                       id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <div class="form-group row mb-0">
-                                        <div class="col-md-8 ml-auto mr-auto justify-content-center">
-                                            <button type="submit" class="btn btn-primary">
-                                                {{ __('Login') }}
-                                            </button>
-
-                                            <div class="mt-3">
-                                                @if (Route::has('password.request'))
-                                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                                        {{ __('Forgot Your Password?') }}
-                                                    </a>
-                                                @endif
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                </form>
+                                <label class="form-check-label" for="remember">
+                                    {{ __('Remember Me') }}
+                                </label>
                             </div>
                         </div>
                     </div>
-                </div>
+
+                    <div class="form-group row mb-0">
+                        <div class="col-md-8 ml-auto mr-auto justify-content-center">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Login') }}
+                            </button>
+
+                            <div class="mt-3">
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
             </div>
-        </section>
+        </div>
     </div>
+
 @endsection
