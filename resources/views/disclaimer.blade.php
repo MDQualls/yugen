27d16ec0ff34d60a174811d846184d323e240b1a
MDQualls <mqualls@gmail.com>
2020-06-01 10:48:48 -0500

    fix margins for mobile on privacy, cookies, and disclaimer pages

--- resources/views/disclaimer.blade.php
+++ resources/views/disclaimer.blade.php
@@ -2,12 +2,13 @@
 
 @section('content')
 
+    <section class="disclaimer-section">
         <div class="row dark-bottom-border">
 
             <h1 class="h1__title">Disclaimer Notice</h1>
             <p class="p__content--lead">
                 Unless otherwise noted, the content on this site in the form of blog posts, photos and other
-                media are the express property of Michael and Holly Qualls, the owners of Yugen Farm.  No consent
+                media are the express property of Michael and Holly Qualls, the owners of Yugen Farm. No consent
                 is given to use content from this site without a notice of our ownership and a link to the content
                 in question unless express permission is given.
             </p>
@@ -23,7 +24,8 @@
 
             <h4 class="h4__title">Privacy Policy</h4>
             <p class="p__content--body">
-                Please see our <a class="body-link" href="{{route('privacy')}}">Privacy Policy</a> for further information about
+                Please see our <a class="body-link" href="{{route('privacy')}}">Privacy Policy</a> for further
+                information about
                 how we collect and use data on our site.
             </p>
 
@@ -32,9 +34,11 @@
         <div class="row">
 
             <p class="p__content--body">
-                <strong>If you feel that we are not abiding by this disclaimer, you should contact us immediately via email.</strong>
+                <strong>If you feel that we are not abiding by this disclaimer, you should contact us immediately via
+                    email.</strong>
             </p>
 
         </div>
+    </section>
 
 @endsection('content')
--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -2,94 +2,95 @@
 
 @section('content')
 
-
-
+    <section class="privacy-section">
         <div class="row dark-bottom-border">
 
-                <h1 class="h1__title" >Privacy Notice</h1>
-                <p class="p__content--lead">
-                    This privacy notice discloses the privacy practices for yugenfarm.com. This privacy notice applies solely to information collected by this website. It will notify you of the following:
-                    What personally identifiable information is collected from you through the website, how it is used and with whom it may be shared.
-                    What choices are available to you regarding the use of your data.
-                    The security procedures in place to protect the misuse of your information.
-                    How you can correct any inaccuracies in the information.
-                </p>
+            <h1 class="h1__title">Privacy Notice</h1>
+            <p class="p__content--lead">
+                This privacy notice discloses the privacy practices for yugenfarm.com. This privacy notice applies
+                solely to information collected by this website. It will notify you of the following:
+                What personally identifiable information is collected from you through the website, how it is used and
+                with whom it may be shared.
+                What choices are available to you regarding the use of your data.
+                The security procedures in place to protect the misuse of your information.
+                How you can correct any inaccuracies in the information.
+            </p>
 
         </div>
         <div class="row">
 
-                <h4 class="h4__title">Information Collection, Use, and Sharing</h4>
-                <p class="p__content--body">
-                    We are the sole owners of the information collected on this site. We only have access to/collect
-                    information that you voluntarily give us via email or other direct contact from you. We will not
-                    sell or rent this information to anyone.
-                </p>
-                <p class="p__content--body">
-                    We will use your information to respond to you, regarding the reason you contacted us. We will
-                    not share your information with any third party outside of our organization, other than as necessary
-                    to fulfill your request, e.g. to ship an order or send you a newsletter.
-                </p>
-                <p class="p__content--body">
-                    Unless you ask us not to, we may contact you via email in the future to tell you about specials,
-                    new products or services, or changes to this privacy policy.
-                </p>
-                <p class="p__content--body">
-                    This website contains links to other sites. Please be aware that we are not responsible for the
-                    content or privacy practices of such other sites. We encourage our users to be aware when they
-                    leave our site and to read the privacy statements of any other site that collects personally
-                    identifiable information.
-                </p>
-
-        </div>
-
-        <div class="row">
-
-                <h4 class="h4__title">Your Access to and Control Over Information</h4>
-                <p class="p__content--body">
-                    You may opt out of any future contacts from us at any time. You can do the following at any time
-                    by contacting us via the email address given on our website:
-                </p>
-                <ul class="user-rights-list">
-                    <li class="user-rights-list__item">&rArr; See what data we have about you, if any.</li>
-                    <li class="user-rights-list__item">&rArr; Change/correct any data we have about you.</li>
-                    <li class="user-rights-list__item">&rArr; Have us delete any data we have about you.</li>
-                    <li class="user-rights-list__item">&rArr; Express any concern you have about our use of your data.</li>
-                    <li class="user-rights-list__item">&rArr; Opt out of any future contact.</li>
-                </ul>
+            <h4 class="h4__title">Information Collection, Use, and Sharing</h4>
+            <p class="p__content--body">
+                We are the sole owners of the information collected on this site. We only have access to/collect
+                information that you voluntarily give us via email or other direct contact from you. We will not
+                sell or rent this information to anyone.
+            </p>
+            <p class="p__content--body">
+                We will use your information to respond to you, regarding the reason you contacted us. We will
+                not share your information with any third party outside of our organization, other than as necessary
+                to fulfill your request, e.g. to ship an order or send you a newsletter.
+            </p>
+            <p class="p__content--body">
+                Unless you ask us not to, we may contact you via email in the future to tell you about specials,
+                new products or services, or changes to this privacy policy.
+            </p>
+            <p class="p__content--body">
+                This website contains links to other sites. Please be aware that we are not responsible for the
+                content or privacy practices of such other sites. We encourage our users to be aware when they
+                leave our site and to read the privacy statements of any other site that collects personally
+                identifiable information.
+            </p>
 
         </div>
 
         <div class="row">
 
-                <h4 class="h4__title">Registration</h4>
-                <p class="p__content--body">
-                    In order to use certain features of this website (such as adding a comment to a blog post),
-                    a user must first complete a registration form. During registration a user is required to give certain
-                    information (such as name and email address). This information may be used to contact you about
-                    products/services on our site.
-                </p>
+            <h4 class="h4__title">Your Access to and Control Over Information</h4>
+            <p class="p__content--body">
+                You may opt out of any future contacts from us at any time. You can do the following at any time
+                by contacting us via the email address given on our website:
+            </p>
+            <ul class="user-rights-list">
+                <li class="user-rights-list__item">&rArr; See what data we have about you, if any.</li>
+                <li class="user-rights-list__item">&rArr; Change/correct any data we have about you.</li>
+                <li class="user-rights-list__item">&rArr; Have us delete any data we have about you.</li>
+                <li class="user-rights-list__item">&rArr; Express any concern you have about our use of your data.</li>
+                <li class="user-rights-list__item">&rArr; Opt out of any future contact.</li>
+            </ul>
 
         </div>
 
         <div class="row">
 
-                <h4 class="h4__title">Security</h4>
-                <p class="p__content--body">
-                    We take precautions to protect your information. When you submit any personal information via the
-                    website, your information is protected both online and offline.
-                </p>
-                <p class="p__content--body">
-                    Wherever we collect personal or sensitive information (such as credit card data), that information
-                    is encrypted and transmitted to us in a secure way. You can verify this by looking for a lock icon
-                    in the address bar and looking for "https" at the beginning of the address of the Web page.
-                </p>
-                <p class="p__content--body">
-                    While we use encryption to protect personal or sensitive information transmitted online, we also
-                    protect your information offline. Only authorized administrators who need the information to
-                    perform a specific job in operating this Web site are granted access to personally identifiable
-                    information. The computers/servers in which we store personally identifiable information are kept
-                    in a secure environment.
-                </p>
+            <h4 class="h4__title">Registration</h4>
+            <p class="p__content--body">
+                In order to use certain features of this website (such as adding a comment to a blog post),
+                a user must first complete a registration form. During registration a user is required to give certain
+                information (such as name and email address). This information may be used to contact you about
+                products/services on our site.
+            </p>
+
+        </div>
+
+        <div class="row">
+
+            <h4 class="h4__title">Security</h4>
+            <p class="p__content--body">
+                We take precautions to protect your information. When you submit any personal information via the
+                website, your information is protected both online and offline.
+            </p>
+            <p class="p__content--body">
+                Wherever we collect personal or sensitive information (such as credit card data), that information
+                is encrypted and transmitted to us in a secure way. You can verify this by looking for a lock icon
+                in the address bar and looking for "https" at the beginning of the address of the Web page.
+            </p>
+            <p class="p__content--body">
+                While we use encryption to protect personal or sensitive information transmitted online, we also
+                protect your information offline. Only authorized administrators who need the information to
+                perform a specific job in operating this Web site are granted access to personally identifiable
+                information. The computers/servers in which we store personally identifiable information are kept
+                in a secure environment.
+            </p>
 
         </div>
 
@@ -101,7 +102,6 @@
             </p>
 
         </div>
-
-
+    </section>
 
 @endsection('content')
