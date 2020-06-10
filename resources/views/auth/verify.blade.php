235e5e86b5d2342e2ca4ca3628ae90b07b74148e
MDQualls <mqualls@gmail.com>
2020-06-10 18:53:17 -0500

    fix bug in verify email

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,31 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('partials._____pagetitle', ['title' => 'Please verify your email'])
-    <div class="container mb100">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
+        <div class="row">
                 <div class="card">
-                    <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                    <div class="card__header">{{ __('Verify Your Email Address') }}</div>
 
-                    <div class="card-body">
+                    <div class="card__body">
                         @if (session('resent'))
-                            <div class="alert alert-success" role="alert">
-                                {{ __('A fresh verification link has been sent to your email address.') }}
+
+                            <div class="alert__container">
+                                <div class="alert__container--alert alert__container--info" role="alert">
+                                    {{ __('A fresh verification link has been sent to your email address.') }}
+                                </div>
                             </div>
+                            <script>
+                                setTimeout(function() {
+                                    jQuery('.alert__container').fadeOut("slow");
+                                }, 5000);
+                            </script>
                         @endif
 
                         {{ __('Before proceeding, please check your email for a verification link.  If you cannot find the email in your inbox or promotions tab please check your spam folder. ') }}
-                        <div class="mb-1 mt-5">
-                            {{ __('If you did not receive the email') }},
+                        <div class="top-margin-rem4">
+                            <div class="bottom-margin-rem2">{{ __('If you did not receive the email') }},</div>
                             <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                                 @csrf
-                                <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                                <button type="submit" class="btn btn--primary ">{{ __('click here to request another') }}</button>.
                             </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 @endsection
