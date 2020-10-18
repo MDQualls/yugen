a6c129ba4154aaf1642aff7a52bb22c99df3b550
MDQualls <mqualls@gmail.com>
2020-10-18 17:51:52 -0500

    start redo of timeline interface

--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -3,25 +3,29 @@
 @section('content')
     <div class="timeline-container">
         <div class="timeline">
+            <div class="row example-centered">
 
-            @foreach($timeline as $entry)
-                <div class="entry">
-                    <div class="title">
-                        <h3>{{Carbon\Carbon::parse($entry->created_at)->format('m/d/Y')}}</h3>
-                        <p>by {{$entry->user->name }}</p>
-                    </div>
-                    <div class="body">
-                        <p>{{$entry->timeline_entry}}</p>
-                        @if($entry->timelineData->count())
-                            <ul>
-                                @foreach($entry->timelineData as $data)
-                                    <li>{{$data->timelineType->timeline_type}} : {{$data->data_entry}}</li>
-                                @endforeach
-                            </ul>
-                        @endif
-                    </div>
+                <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2">
+                    <ul class="timeline timeline-centered">
+                        @foreach($timeline as $entry)
+
+                            <li class="timeline-item">
+                                <div class="timeline-info">
+                                    <span>2020</span>
+                                </div>
+                                <div class="timeline-marker"></div>
+                                <div class="timeline-content">
+                                    <h3 class="timeline-title">
+                                        {{$entry->created_at}}
+                                        <div><small>by {{$entry->user->name}}</small></div>
+                                    </h3>
+                                    <p>{{$entry->timeline_entry}}</p>
+                                </div>
+                            </li>
+
+                        @endforeach
+                    </ul>
                 </div>
-            @endforeach
+            </div>
         </div>
-    </div>
 @endsection('content')
