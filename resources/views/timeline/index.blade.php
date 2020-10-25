4f2d8a6b6ddf1e48804db1b0b7429b63a7505df6
MDQualls <mqualls@gmail.com>
2020-10-25 11:46:31 -0500

    full size timeline layout

--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -1,31 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="timeline-container">
-        <div class="timeline">
-            <div class="row example-centered">
 
-                <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2">
-                    <ul class="timeline timeline-centered">
-                        @foreach($timeline as $entry)
-
-                            <li class="timeline-item">
-                                <div class="timeline-info">
-                                    <span>2020</span>
-                                </div>
-                                <div class="timeline-marker"></div>
-                                <div class="timeline-content">
-                                    <h3 class="timeline-title">
-                                        {{$entry->created_at}}
-                                        <div><small>by {{$entry->user->name}}</small></div>
-                                    </h3>
-                                    <p>{{$entry->timeline_entry}}</p>
-                                </div>
-                            </li>
-
-                        @endforeach
-                    </ul>
-                </div>
+    <div class="row">
+        <div class="card">
+            <div class="card__body">
+                <h1 class="h1__title">Our Timeline.</h1>
+                <p class="p__content--lead">
+                    We decided to add this simple timeline so that we can keep track and look back for posterity.
+                    It will be interesting and instructional to see what we are spending our time and money on
+                    over the years.
+                </p>
             </div>
         </div>
+    </div>
+
+    <div class="timeline-container">
+        <div class="row">
+            <ul class="timeline timeline-centered">
+                @php $year = Carbon\Carbon::parse(now())->format('Y') @endphp
+                @foreach($timeline as $entry)
+
+                    @if($entry->user->name == 'HollyQ')
+                        <li class="timeline-item timeline-item-odd">
+                    @else
+                        <li class="timeline-item timeline-item-even">
+                    @endif
+
+                        @if($year != Carbon\Carbon::parse($entry->created_at)->format('Y') || $loop->first)
+                            <div class="timeline-info">
+                                <span>{{Carbon\Carbon::parse($entry->created_at)->format('Y')}}</span>
+                            </div>
+                        @endif
+                        <div class="timeline-marker"></div>
+                        <div class="timeline-content">
+                            <h3 class="timeline-title">
+                                {{Carbon\Carbon::parse($entry->created_at)->format('m/d/Y g:i a')}}
+                                <div><small>by {{$entry->user->name}}</small></div>
+                            </h3>
+                            <p>
+                                {{$entry->timeline_entry}}
+
+                                @if($entry->timelineData()->count() > 0)
+                                    <ul class="data-type-list">
+                                        @foreach($entry->timelineData as $data)
+                                            <li>
+                                                {{ $data->timelineType->timeline_type }} : {{ $data->data_entry }}
+                                            </li>
+                                        @endforeach
+                                    </ul>
+                                @endif
+                            </p>
+                        </div>
+                    </li>
+                    @php $year = Carbon\Carbon::parse($entry->created_at)->format('Y') @endphp
+                @endforeach
+            </ul>
+        </div>
+    </div>
 @endsection('content')
